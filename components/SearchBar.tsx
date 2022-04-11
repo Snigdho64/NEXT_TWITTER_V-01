8f6031378006cf6a2aafba972a7e605a53dc0a4b
Snigdho64 <79567044+Snigdho64@users.noreply.github.com>
2022-04-11 10:31:36 +0530

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -19,7 +19,7 @@ const SearchBar = () => {
           collection(db, 'users'),
           where('tag', '>=', key),
           where('tag', '<=', key + '\uf8ff'),
-          where('tag', '!=', session?.user.tag)
+          where('uuid', '!=', session?.user.uuid)
           //   where('name', '<=', keyword + '\uf8ff')
         )
       ).then((docs) => {
